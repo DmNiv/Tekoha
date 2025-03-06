507640537d31180999cd27384c22fc30dc4cfba0
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2025-03-05 21:18:47 -0300

    Início refatoração inimigo

---
+++ tekoha/enemies/enemy_ranged/animation_tree.gd
@@ -0,0 +1,7 @@
+extends AnimationTree
+
+@onready var enemy_ranged: Enemy = $".."
+
+func _process(delta: float) -> void:
+	set("parameters/Idle/blend_position", enemy_ranged.last_facing_direction)
+	set("parameters/Walk/blend_position", enemy_ranged.move_direction)
---
+++ tekoha/enemies/enemy_ranged/enemy_ranged.gd
@@ -0,0 +1,18 @@
+class_name Enemy extends CharacterBody2D
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+const SPEED : float = 50.0 
+var move_direction : Vector2
+var last_facing_direction = Vector2(0, 1)
+var idle
+
+func _ready() -> void:
+	animation_tree.active = true
+
+func _physics_process(_delta: float) -> void:
+	idle = !velocity
+	if !idle:
+		last_facing_direction = move_direction
+	
+	move_and_slide()
